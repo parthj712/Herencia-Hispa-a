01f68cf514ce7e4446ee9d74fa30d4c9b79762d5
parth <parth.j@coditude.com>
2025-11-04 11:44:37 +0530

    add hero section

---
+++ src/Comp/MainHome/HeroSection/HeroSection.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className="pb-4">
+            <section
+                className="relative w-full h-[80vh] flex items-end justify-start bg-cover bg-center"
+                style={{ backgroundImage: "url('/flag.jpg')" }}
+            >
+                {/* Optional overlay for contrast */}
+                {/* <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
+
+                {/* Text Content */}
+                <div className="pl-10 md:pl-16 lg:pl-20 relative z-10 max-w-7xl text-left text-white px-8 pb-12">
+                    <h1 className="drop-shadow-[0_2px_3px_rgba(0,0,0,0.6)] text-2xl md:text-3xl lg:text-5xl font-bold mb-4 tracking-[1px] md:tracking-[1.5px]  lg:leading-[60px] md:leading-[40px]">
+                        Unlock Fluent Spanish : Your Next Adventure Starts Today
+                    </h1>
+                    <p className="text-lg md:text-xl drop-shadow-[0_1px_3px_rgba(0,0,0,0.6)]">
+                        Go beyond grammar. Master real-world conversations and immerse yourself
+                        in a vibrant culture through fun, interactive lessons.
+                    </p>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/Comp/MainHome/MainHome.js
+++ src/Comp/MainHome/MainHome.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Welcome from './Welcome/Welcome'
+import HeroSection from './HeroSection/HeroSection'
 
 const MainHome = () => {
   return (
-    <div className='max-w-7xl mx-auto px-4'>
+    <div >
         <Welcome/>
+        <HeroSection/>
     </div>
   )
 }
