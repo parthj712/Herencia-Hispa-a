0f414e7b378af78238035453018d179ca79a9b6e
parth <parth.j@coditude.com>
2025-11-08 21:59:40 +0530

    add home page form

--- src/Comp/MainHome/HeroSection/HeroSection.js
+++ src/Comp/MainHome/HeroSection/HeroSection.js
@@ -13,11 +13,12 @@ const HeroSection = () => {
                 {/* Text Content */}
                 <div className="pl-10 md:pl-16 lg:pl-20 relative z-10 max-w-7xl text-left text-white px-8 pb-12">
                     <h1 className="drop-shadow-[0_2px_3px_rgba(0,0,0,0.6)] text-2xl md:text-3xl lg:text-5xl font-bold mb-4 tracking-[1px] md:tracking-[1.5px]  lg:leading-[60px] md:leading-[40px]">
-                        Unlock Fluent Spanish : Your Next Adventure Starts Today
+                        Unlock Fluent Spanish in India — Your Next Adventure Starts Today
                     </h1>
                     <p className="text-lg md:text-xl drop-shadow-[0_1px_3px_rgba(0,0,0,0.6)]">
-                        Go beyond grammar. Master real-world conversations and immerse yourself
-                        in a vibrant culture through fun, interactive lessons.
+                        Join the best online Spanish classes in India with expert tutors who make learning fun and effective. <br/>
+                        Whether you’re preparing for the DELE Spanish examor starting as a beginner, our
+                        affordable Spanish language courseshelp you master real-world conversations at your own pace.
                     </p>
                 </div>
             </section>
--- src/Comp/MainHome/Testinomials/Testinomials.js
+++ src/Comp/MainHome/Testinomials/Testinomials.js
@@ -23,7 +23,7 @@ const Testimonials = () => {
     };
 
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
         speed: 900,
         slidesToShow: 1,
@@ -62,7 +62,7 @@ const Testimonials = () => {
                 className="relative py-16 bg-gradient-to-l from-[#350D85] to-[#5D17EB] text-center text-white bg-cover bg-center"
                 style={{ backgroundImage: "url('/test_bg.png')" }}
             >
-                <div className="relative max-w-6xl mx-auto px-4 z-10 py-7">
+                <div className="relative max-w-6xl mx-auto px-4 z-10 py-2">
                     <motion.h2
                         className="text-2xl md:text-3xl font-semibold mb-2 tracking-wide"
                         variants={sectionVariants}
@@ -78,7 +78,7 @@ const Testimonials = () => {
                     </motion.p>
 
                     {/* Slider */}
-                    <div className="relative">
+                    <div className="relative py-7">
                         {/* Custom arrows (desktop only) */}
                         <button
                             className="absolute -left-10 top-1/2 transform -translate-y-1/2 hidden lg:flex items-center justify-center w-10 h-10 rounded-full bg-white/20 hover:bg-white/40 transition"
@@ -94,32 +94,47 @@ const Testimonials = () => {
                             <ChevronRight className="text-white" />
                         </button>
 
+                        <span className=" absolute left-6 -top-4 lg:-top-8 md:-top-8 text-[#FDB900] text-[108px] font-bold -z-10 opacity-90">
+                            “
+                        </span>
                         <Slider ref={sliderRef} {...settings}>
                             {testimonialsData.map((test) => (
                                 <motion.div
                                     key={test.id}
-                                    className="px-4 pb-6"
+                                    className="px-4 pb-0 lg:pb-6 md:pb-6"
                                     initial={{ opacity: 0, scale: 0.9 }}
                                     whileInView={{ opacity: 1, scale: 1 }}
                                     transition={{ delay: 0.2, duration: 0.5 }}
                                     viewport={{ once: true }}
                                 >
-                                    <div className="bg-gradient-to-b from-[#fce094] to-[#FDB900] rounded-b-2xl rounded-tr-2xl shadow-lg p-6 text-left text-black">
-                                        <div className="flex items-center gap-4 mb-4">
-                                            <div>
-                                                <h4 className="font-semibold text-[20px] lg:text-2xl md:text-2xl">{test.name}</h4>
-                                                <p className="text-gray-900 text-[16px] lg:text-[18px] md:text-[16px]">
-                                                    {test.location}
-                                                </p>
+                                    <div className="py-16 lg:py-0 md:py-0 relative rounded-b-2xl rounded-tr-2xl p-0 lg:p-6 md:p-6 text-left text-black">
+                                        {/* Opening Quote */}
+
+                                        <div className="px-16 flex flex-col items-center">
+                                            <div className="flex items-center gap-4 mb-4">
+                                                <div>
+                                                    <h4 className="text-[#FDB900] font-semibold text-[26px] lg:text-3xl md:text-2xl text-center">{test.name}</h4>
+                                                    <p className="text-white  text-[16px] lg:text-[18px] md:text-[16px] text-center">
+                                                        {test.location}
+                                                    </p>
+                                                </div>
                                             </div>
+
+                                            <p className="text-white text-center leading-relaxed text-[18px] lg:text-[18px] relative z-10">
+                                                {test.feedback}
+                                            </p>
                                         </div>
-                                        <p className="text-gray-800  leading-relaxed text-[16px] lg:text-[18px]">
-                                            {test.feedback}
-                                        </p>
+
+
                                     </div>
+
                                 </motion.div>
                             ))}
                         </Slider>
+                        {/* Closing Quote */}
+                        <span className="absolute right-6 -bottom-10 lg:-bottom-26 md:-bottom-26 text-[#FDB900] text-[108px] font-bold -z-10 opacity-90">
+                            ”
+                        </span>
                     </div>
 
                     <p className="lg:hidden md:hidden text-[16px] pt-9">Swipe to see more</p>
