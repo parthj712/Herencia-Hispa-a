848ff20238a18eadca9a35c78a17d0401fa31268
yashwalhekar <yashwalhekar371@gmail.com>
2025-11-26 14:02:54 +0530

    tagline api binding

--- src/Comp/TopBar/TopBar.js
+++ src/Comp/TopBar/TopBar.js
@@ -1,23 +1,36 @@
 "use client";
-import React from "react";
+import API from "@/server/api";
+import React, { useEffect, useState } from "react";
 
 export default function TopBar() {
-    return (
-        <div className="w-full bg-indigo-600 text-white py-2 overflow-hidden">
-            <div className="marquee-wrapper">
-                <div className="marquee-content">
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                </div>
+  const [tagline, setTagline] = useState("");
+  const fetchHeadings = async () => {
+    try {
+      const res = await API.get("/tagline/active-tags");
 
-                {/* Duplicate for seamless loop */}
-                <div className="marquee-content">
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                    Batches starting from 1 Jan 2026 — Enroll Now! 🔥 •&nbsp;
-                </div>
+      setTagline(res.data[0].text);
+    } catch (error) {
+      console.log("failed to fetch tags", error.message);
+    }
+  };
+  useEffect(() => {
+    fetchHeadings();
+  }, []);
+  console.log(tagline);
+  return (
+    <div className="w-full bg-indigo-600 text-white py-2 overflow-hidden sticky top-0 z-50 shadow-sm">
+      <div className="marquee-wrapper">
+        {(tagline ? tagline : "Loading Announcement...") && (
+          <>
+            <div className="marquee-content">
+              {tagline} &nbsp; {tagline} &nbsp; {tagline} &nbsp;
             </div>
-        </div>
-    );
+            <div className="marquee-content">
+              {tagline} &nbsp; {tagline} &nbsp; {tagline} &nbsp;
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
